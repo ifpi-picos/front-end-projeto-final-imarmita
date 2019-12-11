e9ad4d4d55d000a5686f371bbd7e45c3984d33c7
KarolaineAlencar <kalencar98@gmail.com>
2019-12-11 18:52:06 -0300

    finalização das ultimas telas e css correspondetes

---
+++ js/inserirPrato.js
@@ -0,0 +1,10 @@
+
+function inserirItem(){
+    console.log("entrrei")
+    const inputsPrato = document.getElementById("inputsPrato");
+
+    let newInput = document.createElement("INPUT");
+    newInput.setAttribute("type", "text");
+    newInput.setAttribute("placeholder", "Item do prato");
+    inputsPrato.appendChild(newInput);
+}
